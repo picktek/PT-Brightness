a4ae7a9cffe411911cf8e0340f7861c37f6adac7
luka dodelia <luka@picktek.com>
2019-01-10 22:53:11 +0400

    init

---
+++ PT Brigtness/PanelController.m
@@ -0,0 +1,243 @@
+#import "PanelController.h"
+#import "BackgroundView.h"
+#import "StatusItemView.h"
+#import "MenubarController.h"
+#import "ddc.h"
+
+#define OPEN_DURATION .15
+#define CLOSE_DURATION .1
+
+#define POPUP_HEIGHT 180
+#define PANEL_WIDTH 280
+#define MENU_ANIMATION_DURATION .2
+
+#pragma mark -
+
+@implementation PanelController
+
+@synthesize backgroundView = _backgroundView;
+@synthesize delegate = _delegate;
+
+#pragma mark -
+
+- (id)initWithDelegate:(id<PanelControllerDelegate>)delegate
+{
+    self = [super initWithWindowNibName:@"Panel"];
+    if (self != nil)
+    {
+        _delegate = delegate;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+  
+    
+}
+
+- (void)setControlValue:(int)control :(int)value {
+    struct DDCWriteCommand write_command;
+    write_command.control_id = control;
+    write_command.new_value = value;
+    ddc_write(0, &write_command);
+}
+
+
+- (int)readControlValue:(int)control {
+    struct DDCReadCommand read_command;
+    read_command.control_id = control;
+    
+    ddc_read(0, &read_command);
+    return ((int)read_command.response.current_value);
+}
+
+
+- (void)setControlsToCurrentValues
+{
+    //Brightness
+    int brightness_value = [self readControlValue:0x10];
+    [_brightSlider setIntValue:brightness_value];
+    [_brightLabelValue setIntValue:brightness_value];
+    
+    //Contrast
+    int contrast_value = [self readControlValue:0x12];
+    [_contrastSlider setIntValue:contrast_value];
+    [_contrastLabelValue setIntValue:contrast_value];
+}
+
+
+- (void)setControlAndUpdateLabel:(int)control :(id)slider
+{
+    [self setControlValue:control :[slider intValue]];
+}
+
+#pragma mark -
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    // Make a fully skinned panel
+    NSPanel *panel = (id)[self window];
+    [panel setAcceptsMouseMovedEvents:YES];
+    [panel setLevel:NSPopUpMenuWindowLevel];
+    [panel setOpaque:NO];
+    [panel setBackgroundColor:[NSColor clearColor]];
+}
+
+#pragma mark - Public accessors
+
+- (BOOL)hasActivePanel
+{
+    return _hasActivePanel;
+}
+
+- (void)setHasActivePanel:(BOOL)flag
+{
+    if (_hasActivePanel != flag)
+    {
+        _hasActivePanel = flag;
+        
+        if (_hasActivePanel)
+        {
+            [self openPanel];
+        }
+        else
+        {
+            [self closePanel];
+        }
+    }
+}
+
+#pragma mark - NSWindowDelegate
+
+- (void)windowWillClose:(NSNotification *)notification
+{
+    self.hasActivePanel = NO;
+}
+
+- (void)windowDidResignKey:(NSNotification *)notification;
+{
+    if ([[self window] isVisible])
+    {
+        self.hasActivePanel = NO;
+    }
+}
+
+
+#pragma mark - Public methods
+
+- (NSRect)statusRectForWindow:(NSWindow *)window
+{
+    NSRect screenRect = [[[NSScreen screens] objectAtIndex:0] frame];
+    NSRect statusRect = NSZeroRect;
+    
+    StatusItemView *statusItemView = nil;
+    if ([self.delegate respondsToSelector:@selector(statusItemViewForPanelController:)])
+    {
+        statusItemView = [self.delegate statusItemViewForPanelController:self];
+    }
+    
+    if (statusItemView)
+    {
+        statusRect = statusItemView.globalRect;
+        statusRect.origin.y = NSMinY(statusRect) - NSHeight(statusRect);
+    }
+    else
+    {
+        statusRect.size = NSMakeSize(STATUS_ITEM_VIEW_WIDTH, [[NSStatusBar systemStatusBar] thickness]);
+        statusRect.origin.x = roundf((NSWidth(screenRect) - NSWidth(statusRect)) / 2);
+        statusRect.origin.y = NSHeight(screenRect) - NSHeight(statusRect) * 2;
+    }
+    return statusRect;
+}
+
+- (void)windowDidResize:(NSNotification *)notification
+{
+    NSWindow *panel = [self window];
+    NSRect statusRect = [self statusRectForWindow:panel];
+    NSRect panelRect = [panel frame];
+    
+    CGFloat statusX = roundf(NSMidX(statusRect));
+    CGFloat panelX = statusX - NSMinX(panelRect);
+    
+    self.backgroundView.arrowX = panelX;
+}
+
+- (void)openPanel
+{
+    NSWindow *panel = [self window];
+    
+    NSRect screenRect = [[[NSScreen screens] objectAtIndex:0] frame];
+    NSRect statusRect = [self statusRectForWindow:panel];
+
+    NSRect panelRect = [panel frame];
+    panelRect.size.width = PANEL_WIDTH;
+    panelRect.size.height = POPUP_HEIGHT;
+    panelRect.origin.x = roundf(NSMidX(statusRect) - NSWidth(panelRect) / 2);
+    panelRect.origin.y = NSMaxY(statusRect) - NSHeight(panelRect);
+    
+    if (NSMaxX(panelRect) > (NSMaxX(screenRect) - ARROW_HEIGHT))
+        panelRect.origin.x -= NSMaxX(panelRect) - (NSMaxX(screenRect) - ARROW_HEIGHT);
+    
+    [NSApp activateIgnoringOtherApps:NO];
+    [panel setAlphaValue:0];
+    [panel setFrame:statusRect display:YES];
+    [panel makeKeyAndOrderFront:nil];
+    
+    NSTimeInterval openDuration = OPEN_DURATION;
+    
+    NSEvent *currentEvent = [NSApp currentEvent];
+    if ([currentEvent type] == NSLeftMouseDown)
+    {
+        NSUInteger clearFlags = ([currentEvent modifierFlags] & NSDeviceIndependentModifierFlagsMask);
+        BOOL shiftPressed = (clearFlags == NSShiftKeyMask);
+        BOOL shiftOptionPressed = (clearFlags == (NSShiftKeyMask | NSAlternateKeyMask));
+        if (shiftPressed || shiftOptionPressed)
+        {
+            openDuration *= 10;
+            
+            if (shiftOptionPressed)
+                NSLog(@"Icon is at %@\n\tMenu is on screen %@\n\tWill be animated to %@",
+                      NSStringFromRect(statusRect), NSStringFromRect(screenRect), NSStringFromRect(panelRect));
+        }
+    }
+    
+    [NSAnimationContext beginGrouping];
+    [[NSAnimationContext currentContext] setDuration:openDuration];
+    [[panel animator] setFrame:panelRect display:YES];
+    [[panel animator] setAlphaValue:1];
+    [NSAnimationContext endGrouping];
+    
+    [self setControlsToCurrentValues];
+}
+
+- (void)closePanel
+{
+    [NSAnimationContext beginGrouping];
+    [[NSAnimationContext currentContext] setDuration:CLOSE_DURATION];
+    [[[self window] animator] setAlphaValue:0];
+    [NSAnimationContext endGrouping];
+    
+    dispatch_after(dispatch_walltime(NULL, NSEC_PER_SEC * CLOSE_DURATION * 2), dispatch_get_main_queue(), ^{
+        
+        [self.window orderOut:nil];
+    });
+}
+
+- (IBAction)setBrightness:(id)sender {
+    [_brightLabelValue setIntValue:[sender intValue]];
+    [self setControlAndUpdateLabel:0x10 :sender];    
+}
+
+- (IBAction)setContrast:(id)sender {
+    [_contrastLabelValue setIntValue:[sender intValue]];
+    [self setControlAndUpdateLabel:0x12 :sender];
+}
+
+- (IBAction)quitApp:(id)sender {
+    [NSApp terminate:sender];
+}
+
+@end
